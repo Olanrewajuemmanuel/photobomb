42a33afcd3c9b45983636726ea1b073f806dc660
larry <olalerulanre@gmail.com>
2021-03-25 16:32:50 +0100

    making templates from html files -- hope it works

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,30 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-    mode: "development",
-    devtool: "source-map",
-    entry: './src/index.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    module: {
-        rules: [{test: /\.js/, use: 'babel-loader', exclude: '/node_modules/'}]
-    },
-    resolve: {
-        extensions: ['.jsx', '.js']
-    },
-    plugins: [new CleanWebpackPlugin()]
-}
+  mode: "development",
+  devtool: "source-map",
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [{ test: /\.jsx?/, use: "babel-loader", exclude: "/node_modules/" }],
+  },
+  resolve: {
+    extensions: [".jsx", ".js"],
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "/public"),
+    hot: true,
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "template.html",
+      filename: "../public/index.html",
+    }),
+  ],
+};
